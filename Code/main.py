f531914d60c376d101ae61bfb492f0657486662a
bonscji1 <ji.bonsch@seznam.cz>
2022-12-16 16:05:26 +0100

    preparation for comparative testing

--- Code/main.py
+++ Code/main.py
@@ -18,6 +18,27 @@ def print_output(text):
     print(text)
     print("----------******************************-------------")
 
+def testing_easy_ocr():
+    reader = easyocr.Reader(['en'])
+
+    # read image
+    img0 = cv2.imread('Images/Test.jpg')
+
+    output = reader.readtext(img0, detail=0)  # detail 0 for simple output, 1 for expanded
+
+    # options
+    # width_ths (float, default = 0.5) - Maximum horizontal distance to merge boxes.
+
+    print("EasyOCR output:")
+    print_output(output)
+
+
+def test_easy_ocr(img, config):
+    reader = easyocr.Reader(config)
+
+    return reader.readtext(img, detail=0)
+
+
 
 def main_easy_ocr():
     # downloads packages, takes a while on the first run
@@ -37,7 +58,7 @@ def main_easy_ocr():
 
 def main_tesseract():
     # configurations
-    config = ('-l spa+ces --oem 1 --psm 3')
+    config = '-l spa+ces --oem 1 --psm 3'
 
     # read image
     img = cv2.imread('Images/page1.jpg')
@@ -52,42 +73,46 @@ def main_tesseract():
     print_output(text)
 
 
+def test_tesseract(img, config):
+    prep_img = preprocess(img)
+
+    return pytesseract.image_to_string(prep_img, config=config)
+
+
 def testing_tesseract():
     # configurations
-    config = ('-l eng --oem 1 --psm 3')
+    config = '-l eng --oem 1 --psm 3'
 
     # read image
     img0 = cv2.imread('Images/Test.jpg')
     img1 = cv2.imread('Images/test_1.png')
     img2 = cv2.imread('Images/test_2.png')
+    img3 = cv2.imread('Images/page_test.jpg')
 
-    pimg0 = preprocess(img0)
-    pimg1 = preprocess(img1)
-    pimg2 = preprocess(img2)
+    # text0 = test_tesseract(img0, config)
+    # text1 = test_tesseract(img1, config)
+    # text2 = test_tesseract(img2, config)
 
-    text0 = pytesseract.image_to_string(pimg0, config=config)
-    text1 = pytesseract.image_to_string(pimg1, config=config)
-    text2 = pytesseract.image_to_string(pimg2, config=config)
+    config = '-l spa+ces --oem 1 --psm 3'
+    text3 = test_tesseract(img3, config)
 
     print("Tesseract output:")
-    print_output(text0)
+    # print_output(text0)
     # print_output(text1)
     # print_output(text2)
+    print_output(text3)
 
 
-def testing_easy_ocr():
-    reader = easyocr.Reader(['en'])
 
-    # read image
-    img0 = cv2.imread('Images/Test.jpg')
+def comparative_testing():
+    # set testing image
+    img = cv2.imread('Images/Test.jpg')
 
-    output = reader.readtext(img0, detail=0)  # detail 0 for simple output, 1 for expanded
+    # set testing configurations
+    config_tes = '-l spa+ces --oem 1 --psm 3'
+    config_eocr = ['cs', 'es']
 
-    # options
-    # width_ths (float, default = 0.5) - Maximum horizontal distance to merge boxes.
 
-    print("EasyOCR output:")
-    print_output(output)
 
 
 if __name__ == '__main__':
@@ -95,4 +120,4 @@ if __name__ == '__main__':
     main_easy_ocr()
 
     # testing_tesseract()
-    #testing_easy_ocr()
+    # testing_easy_ocr()
